52e59ba8d9670f07a063b4c8c0e542c162541885
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2024-12-06 09:42:06 +0300

    files arrangement

---
+++ Testimonial Slider/script.js
@@ -0,0 +1,8 @@
+// 1. Declare storage medium for the images
+let imagesCenter = ["images/buggatti.jpeg","images/camero.jpeg","images/lambo.jpeg"];
+// 2. Declare storage medium for the testmoniar statements
+let statements = ["buggatti is the best car from europe, cheers","Americans got a great idea on developing camero","lambo is just super fast"];
+
+// 3. Set amount of time required for each statement to appear
+alert("yelele");
+// 4. Write logic to switch between the statements
